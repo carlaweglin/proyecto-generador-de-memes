ba924c4a7de0c141ad7b6a77d2d06258b9806640
carlaweglin <weglin.carla@gmail.com>
2022-11-13 13:53:57 -0300

    7mo commit-aside-texto-funcionabilidad-textos

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 let resetearFiltros = () => {
-    inputControlShiny.value = 1;
-    inputControlOpacity.value = 100;
-    inputControlContrast.value = 100;
-    inputControlBlur.value = 0;
-    inputControlGrayscale.value = 0;
-    inputControlSaturation.value = 100;
-    inputControlNegative.value = 0;
-    inputControlSepia.value = 0;
-    containerMeme.style.filter = `brightness(${inputControlShiny.value}) contrast(${inputControlContrast.value}%) blur(${inputControlBlur.value}px) grayscale(${inputControlGrayscale.value}%) saturate(${inputControlSaturation.value}%) invert(${inputControlNegative.value}%) sepia(${inputControlSepia.value}%)`;
-    containerMeme.style.opacity = `${inputControlOpacity.value}%`;
+  inputControlShiny.value = 1;
+  inputControlOpacity.value = 100;
+  inputControlContrast.value = 100;
+  inputControlBlur.value = 0;
+  inputControlGrayscale.value = 0;
+  inputControlSaturation.value = 100;
+  inputControlNegative.value = 0;
+  inputControlSepia.value = 0;
+  containerMeme.style.filter = `brightness(${inputControlShiny.value}) contrast(${inputControlContrast.value}%) blur(${inputControlBlur.value}px) grayscale(${inputControlGrayscale.value}%) saturate(${inputControlSaturation.value}%) invert(${inputControlNegative.value}%) sepia(${inputControlSepia.value}%)`;
+  containerMeme.style.opacity = `${inputControlOpacity.value}%`;
 };
 
 //------------------------------------------------------------------//
@@ -36,17 +36,23 @@ let inputControlSaturation = $("#range-saturation");
 let inputControlNegative = $("#range-negative");
 let inputControlSepia = $("#range-sepia");
 let btnResetValues = $("#reset-values");
+let inputTopText = $("#top-text");
+let inputBottomText = $("#text-bottom");
+let texTop = $(".top-text");
+let bottomText = $(".bottom-text");
+let checkToptext = $('#check-toptext');
+let checkBottomtext = $('#check-bottomtext');
 
-console.log();
+console.log(texTop);
 
 //FUNCIONES //
 
 // funcion para descargar meme //
 
 const descargarMeme = () => {
-    domtoimage.toBlob(contenedorImagen).then(function (blob) {
-        saveAs(blob, "mi-meme.jpg");
-    });
+  domtoimage.toBlob(contenedorImagen).then(function (blob) {
+    saveAs(blob, "mi-meme.jpg");
+  });
 }; // cuando ejecuto funcion me deja de funcionar la imagen ?? //
 
 //------------------------------------------------------------------//
@@ -56,109 +62,130 @@ const descargarMeme = () => {
 // visualizacion de aside imagen y texto //
 
 btnText.addEventListener("click", () => {
-    console.log("aprete boton texto");
-    asideImage.classList.add("display-none");
-    asideText.classList.remove("display-none");
+  asideImage.classList.add("display-none");
+  asideText.classList.remove("display-none");
 });
 
 btnImage.addEventListener("click", () => {
-    console.log("aprete boton img");
-    asideText.classList.add("display-none");
-    asideImage.classList.remove("display-none");
+  asideText.classList.add("display-none");
+  asideImage.classList.remove("display-none");
 });
 
 // insertar imagen en div //
 
 inputUrl.addEventListener("input", (event) => {
-    let url = event.target.value;
-    containerMeme.style.backgroundImage = `url(${url})`;
+  let url = event.target.value;
+  containerMeme.style.backgroundImage = `url(${url})`;
 });
 
 // cambiar color de fondo contenedor //
 
 inputBackgroundColor.addEventListener("change", (event) => {
-    let colorBack = event.target.value;
-    containerMeme.style.backgroundColor = colorBack;
+  let colorBack = event.target.value;
+  containerMeme.style.backgroundColor = colorBack;
 });
 
 // Cambiar modo de mezcla de fondo //
 
 selectBlendMode.addEventListener("click", (event) => {
-    value = event.target.value;
-    switch (value) {
-        case "ninguno":
-            containerMeme.style.backgroundBlendMode = "normal";
-            break;
-        case "aclarar":
-            containerMeme.style.backgroundBlendMode = "lighten";
-            break;
-        case "oscurecer":
-            containerMeme.style.backgroundBlendMode = "darken";
-            break;
-        case "diferencia":
-            containerMeme.style.backgroundBlendMode = "difference";
-            break;
-        case "luminosidad":
-            containerMeme.style.backgroundBlendMode = "luminosity";
-            break;
-        case "multiplicar":
-            containerMeme.style.backgroundBlendMode = "multiply";
-            break;
-    }
+  value = event.target.value;
+  switch (value) {
+    case "ninguno":
+      containerMeme.style.backgroundBlendMode = "normal";
+      break;
+    case "aclarar":
+      containerMeme.style.backgroundBlendMode = "lighten";
+      break;
+    case "oscurecer":
+      containerMeme.style.backgroundBlendMode = "darken";
+      break;
+    case "diferencia":
+      containerMeme.style.backgroundBlendMode = "difference";
+      break;
+    case "luminosidad":
+      containerMeme.style.backgroundBlendMode = "luminosity";
+      break;
+    case "multiplicar":
+      containerMeme.style.backgroundBlendMode = "multiply";
+      break;
+  }
 });
 
 // cambiar filtros //
 
 //brillo//
 inputControlShiny.addEventListener("change", (event) => {
-    containerMeme.style.filter = `brightness(${event.target.value})`;
+  containerMeme.style.filter = `brightness(${event.target.value})`;
 });
 
 //opacidad ///
 
 inputControlOpacity.addEventListener("change", (event) => {
-    containerMeme.style.opacity = `${event.target.value}%`;
+  containerMeme.style.opacity = `${event.target.value}%`;
 });
 
 //contraste//
 
 inputControlContrast.addEventListener("change", (event) => {
-    containerMeme.style.filter = `contrast(${event.target.value}%)`;
+  containerMeme.style.filter = `contrast(${event.target.value}%)`;
 });
 
 //desenfoque//
 
 inputControlBlur.addEventListener("change", (event) => {
-    containerMeme.style.filter = `blur(${event.target.value}px)`;
+  containerMeme.style.filter = `blur(${event.target.value}px)`;
 });
 
 //escala de grises //
 
 inputControlGrayscale.addEventListener("change", (event) => {
-    containerMeme.style.filter = `grayscale(${event.target.value}%)`;
+  containerMeme.style.filter = `grayscale(${event.target.value}%)`;
 });
 
 //saturado//
 
 inputControlSaturation.addEventListener("change", (event) => {
-    containerMeme.style.filter = `saturate(${event.target.value}%)`;
+  containerMeme.style.filter = `saturate(${event.target.value}%)`;
 });
 
 //negativo//
 
 inputControlNegative.addEventListener("change", (event) => {
-    containerMeme.style.filter = `invert(${event.target.value}%)`;
+  containerMeme.style.filter = `invert(${event.target.value}%)`;
 });
 
 //sepia//
 
 inputControlSepia.addEventListener("change", (event) => {
-    containerMeme.style.filter = `sepia(${event.target.value}%)`;
-    console.log("funciona");
+  containerMeme.style.filter = `sepia(${event.target.value}%)`;
 });
 
 //boton resetear filtros//
 
 btnResetValues.addEventListener("click", resetearFiltros);
+
+// top y bottom text //
+
+inputTopText.addEventListener("input", (event) => {
+  texTop.innerText = event.target.value;
+});
+
+inputBottomText.addEventListener("input", (event) => {
+  bottomText.innerText = event.target.value;
+});
+
+// con/sin texto //
+
+checkToptext.addEventListener('click', () => {
+    texTop.classList.toggle("display-none"); 
+})
+
+checkBottomtext.addEventListener('click', () => {
+    bottomText.classList.toggle("display-none"); 
+})
+
+
+
+
 
 // Modo claro, modo Oscuro //
